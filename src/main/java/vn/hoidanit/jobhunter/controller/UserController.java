a9e97d4f300e2300817690e4a68e12a6a3d31841
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2024-12-07 10:58:18 +0700

    #34

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -29,4 +33,23 @@ public class UserController {
     this.userService.handleDeleteUserByID(id);
     return "Delete user";
   }
+
+  @GetMapping("/user/{id}")
+  public User getUserByID(@PathVariable("id") long id) {
+    User user = this.userService.fetchUserByID(id);
+    return user;
+  }
+
+  @GetMapping("/user")
+  public List<User> getAllUsers() {
+    List<User> users = this.userService.fetchAllUsers();
+    return users;
+  }
+
+  @PutMapping("/user")
+  public User updateUser(@RequestBody User postManUser) {
+
+    return this.userService.handleUpdateUser(postManUser);
+  }
+
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -19,5 +22,28 @@ public class UserService {
 
   public void handleDeleteUserByID(long id) {
     this.userRepository.deleteById(id);
+  }
+
+  public User fetchUserByID(long id) {
+    Optional<User> userOptional = this.userRepository.findById(id);
+    if (userOptional.isPresent()) {
+      return userOptional.get();
+    }
+    return null;
+  }
+
+  public List<User> fetchAllUsers() {
+    return this.userRepository.findAll();
+  }
+
+  public User handleUpdateUser(User user) {
+    User currentUser = this.fetchUserByID(user.getId());
+    if (currentUser != null) {
+      currentUser.setEmail(user.getEmail());
+      currentUser.setName(user.getName());
+      currentUser.setPassword(user.getPassword());
+      currentUser = this.userRepository.save(currentUser);
+    }
+    return currentUser;
   }
 }

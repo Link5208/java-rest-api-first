695009d48c0ed05e0ad9306bc24cabad9eb05b4a
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2024-12-07 15:28:44 +0700

    #41

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -31,7 +34,10 @@ public class UserController {
   }
 
   @DeleteMapping("/users/{id}")
-  public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+  public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+    if (id >= 1500) {
+      throw new IdInvalidException("Id isn't bigger than 1500");
+    }
     this.userService.handleDeleteUserByID(id);
     return ResponseEntity.ok("Delete user");
     // return ResponseEntity.status(HttpStatus.OK).body("Delete user");

c0369ce46bede67435c088f6bb7af3856e439e83
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-02-06 15:45:35 +0700

    #93

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -1,12 +1,15 @@
 package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -29,19 +32,17 @@ public class Company {
 
 	@Column(columnDefinition = "MEDIUMTEXT")
 	private String description;
-
 	private String address;
-
 	private String logo;
 
 	// @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+7")
 	private Instant createdAt;
-
 	private Instant updatedAt;
-
 	private String createdBy;
-
 	private String updatedBy;
+
+	@OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+	private List<User> users;
 
 	@PrePersist
 	public void handleBeforeCreate() {

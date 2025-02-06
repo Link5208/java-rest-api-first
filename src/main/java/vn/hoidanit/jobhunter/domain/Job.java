ba0eccd17f7212317f4a5958f158960226500cc3
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-02-06 22:56:40 +0700

    #95

--- src/main/java/vn/hoidanit/jobhunter/domain/Job.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Job.java
@@ -7,49 +7,60 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
+import vn.hoidanit.jobhunter.util.constant.LevelEnum;
 
 @Entity
-@Table(name = "companies")
-@Getter
+@Table(name = "jobs")
 @Setter
-public class Company {
+@Getter
+public class Job {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-
-	@NotBlank(message = "Name is not blank")
 	private String name;
+	private String location;
+	private double salary;
+	private int quantity;
+
+	@Enumerated(EnumType.STRING)
+	private LevelEnum level;
 
 	@Column(columnDefinition = "MEDIUMTEXT")
 	private String description;
-	private String address;
-	private String logo;
 
-	// @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+7")
+	private Instant startDate;
+	private Instant endDate;
+	private boolean isActive;
 	private Instant createdAt;
 	private Instant updatedAt;
 	private String createdBy;
 	private String updatedBy;
 
-	@OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
-	@JsonIgnore
-	private List<User> users;
+	@ManyToOne
+	@JoinColumn(name = "company_id")
+	private Company company;
 
-	@OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+	@ManyToMany(fetch = FetchType.LAZY)
 	@JsonIgnore
-	private List<Job> jobs;
+	@JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+	private List<Skill> skills;
 
 	@PrePersist
 	public void handleBeforeCreate() {

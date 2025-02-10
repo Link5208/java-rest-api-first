49ba751049db7ba17347b4924afbe171717a555f
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-02-10 21:57:33 +0700

    #103

--- src/main/java/vn/hoidanit/jobhunter/domain/Job.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Job.java
@@ -3,6 +3,7 @@ package vn.hoidanit.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
@@ -17,6 +18,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -62,9 +64,12 @@ public class Job {
 
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JsonIgnoreProperties(value = { "jobs" })
-	// @JsonIgnore
 	@JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
 	private List<Skill> skills;
+
+	@OneToMany(mappedBy = "job", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Resume> resumes;
 
 	@PrePersist
 	public void handleBeforeCreate() {

07e3965476d59b50eab2271b57ae14071aaa1a74
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-02-10 21:36:13 +0700

    #102

--- src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/FileController.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.Instant;
@@ -7,6 +8,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +23,7 @@ import vn.hoidanit.jobhunter.domain.response.file.ResUploadFileDTO;
 import vn.hoidanit.jobhunter.service.FileService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 import vn.hoidanit.jobhunter.util.error.StorageException;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -39,14 +45,18 @@ public class FileController {
 	@ApiMessage("Upload single file")
 	public ResponseEntity<ResUploadFileDTO> uploadFile(
 			@RequestParam(name = "file", required = false) MultipartFile file,
-			@RequestParam("folder") String folder) throws URISyntaxException, IOException, StorageException {
+			@RequestParam(name = "folder", required = false) String folder)
+			throws URISyntaxException, IOException, StorageException {
 
 		// skip validation
-		if (file == null || file.isEmpty()) {
+		if (folder == null || file.isEmpty()) {
 			throw new StorageException("File is empty. Please upload a file");
 		}
 
 		String fileName = file.getOriginalFilename();
+		if (fileName == null) {
+			throw new StorageException("Empty filename");
+		}
 		List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc");
 		boolean isValid = allowedExtensions.stream().anyMatch(extension -> fileName.toLowerCase().endsWith(extension));
 		if (!isValid) {
@@ -57,4 +67,27 @@ public class FileController {
 		ResUploadFileDTO dto = new ResUploadFileDTO(uploadFile, Instant.now());
 		return ResponseEntity.ok(dto);
 	}
+
+	@GetMapping("/files")
+	public ResponseEntity<Resource> downloadFile(
+			@RequestParam(name = "fileName", required = false) String fileName,
+			@RequestParam(name = "folder", required = false) String folder)
+			throws StorageException, URISyntaxException, FileNotFoundException {
+		if (fileName == null || folder == null) {
+			throw new StorageException("Missing required params!");
+		}
+
+		long fileLength = this.fileService.getFileLength(fileName, folder);
+		if (fileLength == 0) {
+			throw new StorageException("File with name = " + fileName + " is not found!");
+		}
+
+		InputStreamResource resource = this.fileService.getResource(fileName, folder);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+				.contentLength(fileLength)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(resource);
+	}
+
 }
--- src/main/java/vn/hoidanit/jobhunter/service/FileService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/FileService.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,6 +13,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -46,5 +49,26 @@ public class FileService {
 			Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
 		}
 		return finalName;
+	}
+
+	public long getFileLength(String fileName, String folder) throws URISyntaxException {
+		URI uri = new URI(baseURI + folder + "/" + fileName);
+		Path path = Paths.get(uri);
+		File file = new File(path.toString());
+
+		if (!file.exists() || file.isDirectory()) {
+			return 0;
+		}
+		return file.length();
+	}
+
+	public InputStreamResource getResource(String fileName, String folder)
+			throws URISyntaxException, FileNotFoundException {
+		// TODO Auto-generated method stub
+		URI uri = new URI(baseURI + folder + "/" + fileName);
+		Path path = Paths.get(uri);
+
+		File file = new File(path.toString());
+		return new InputStreamResource(new FileInputStream(file));
 	}
 }
